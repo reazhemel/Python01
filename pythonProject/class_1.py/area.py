b5778c04d70769ce171f2c9dccb1bc1d1e6a5d1a
Hemel <47193108+reazhemel@users.noreply.github.com>
2021-10-26 15:44:30 +0600

    Add files via upload

--- pythonProject/class_1.py/area.py
+++ pythonProject/class_1.py/area.py
@@ -1,2 +1,10 @@
-# Python01
-Python Beginners to Advanced
+import phonenumbers
+from class_1 import number
+
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "en"))
+
+from phonenumbers import carrier
+operator_name = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(operator_name, "RO"))
